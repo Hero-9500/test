07d437c3324b9d8ed791e4771212e407b1d53da3
Hiren Ramani <ramanihiren77@Gmail.com>
2023-01-24 21:00:06 +0530

    Update vehicle_info.dart

--- lib/presentation/splash/vehicle_info.dart
+++ lib/presentation/splash/vehicle_info.dart
@@ -18,81 +18,83 @@ class _VehicleInfoState extends State<VehicleInfo> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              animated = !animated;
-            });
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.yellow[50],
-              borderRadius: BorderRadius.circular(8),
-            ),
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                AnimatedTextKit(
-                  animatedTexts: [
-                    TypewriterAnimatedText(
-                      widget.vehicle['name'],
-                      textStyle: const TextStyle(
-                        fontSize: 32.0,
-                        fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                animated = !animated;
+              });
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.yellow[50],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        widget.vehicle['name'],
+                        textStyle: const TextStyle(
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        speed: const Duration(milliseconds: 100),
                       ),
-                      speed: const Duration(milliseconds: 100),
-                    ),
-                  ],
-                  repeatForever: true,
-                  pause: const Duration(milliseconds: 10),
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      widget.vehicle['model'],
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey[800],
+                    ],
+                    repeatForever: true,
+                    pause: const Duration(milliseconds: 10),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.vehicle['model'],
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[800],
+                        ),
                       ),
-                    ),
-                    Text(
-                      widget.vehicle['manufacturer'],
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey[800],
+                      Text(
+                        widget.vehicle['manufacturer'],
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[800],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'passengers: ${widget.vehicle['passengers']}',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey[800],
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'passengers: ${widget.vehicle['passengers']}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[800],
+                        ),
                       ),
-                    ),
-                    Text(
-                      'Lenght: ${widget.vehicle['length']}',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey[800],
+                      Text(
+                        'Lenght: ${widget.vehicle['length']}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[800],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
